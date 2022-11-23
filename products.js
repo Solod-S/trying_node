2b6b0dc8d35597e291ae7210d80986813f574a09
Solod Sergey <104456968+Solod-S@users.noreply.github.com>
2022-11-23 09:53:15 +0200

    +

---
+++ products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    _id: 1,
+    name: "iphone 5x",
+    price: 3400,
+  },
+  {
+    _id: 2,
+    name: "macbook Pro 15",
+    price: 12000,
+  },
+];
+
+module.exports = products;
--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const { router } = require("./booksRouter");
-
+const got = require("got-promise");
 const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
 
 const PORT = 8801;
+const weatherBaseUrl = "http://api.weatherbit.io/v2.0/current";
+// got("google.com")
+//   .then(function (res) {
+//     console.log(res.body);
+//   })
+//   .catch(function (err) {
+//     console.error(err);
+//     console.log(err.response.body);
+//   });
+
+// got.post("google.com"); // => Promise
+//?key=f5a1a09f37fa482d9822dc6cf7b9e5e2&lat=50.519630&lon=30.787088
+app.get("/api/weather", async (req, res) => {
+  try {
+    // const response = await got(
+    //   `api.weatherbit.io/v2.0/current?key=f5a1a09f37fa482d9822dc6cf7b9e5e2&lat=50.519630&lon=30.787088`
+    // );
+    // const response = await got(weatherBaseUrl, {
+    //   key: "f5a1a09f37fa482d9822dc6cf7b9e5e2",
+    //   lat: "50.51963",
+    //   lon: "30.787088",
+    // });
+    const { body } = await got.get(
+      "api.weatherbit.io/v2.0/current?key=f5a1a09f37fa482d9822dc6cf7b9e5e2&lat=50.519630&lon=30.787088"
+    );
+    // const redy = await body.json();
+    console.log(body);
+    return res.json({ response: body });
+  } catch (error) {
+    res.status(500).json({ messege: error.msg });
+  }
+});
 
 app.use(router);
 // рутер для масштабирования приложения гет запрос http://localhost:8081/books вернет массив, app.use("/api", router) => http://localhost:8081/api/books
